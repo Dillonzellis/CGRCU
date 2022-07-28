9da9821c7965ed52a53ca7cf8b9bd8b9a6180bb9
Dillon Ellis <dellis@fcul.com>
2022-07-28 13:54:20 -0400

    original fee-schedule page

---
+++ pages/fee-schedule.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<title>CGR Credit Union - Fee Schedule</title>
+
+<!--META DATA-->
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<meta name="description" content="Like all credit unions, CGR Credit Union is a not-for-profit financial cooperative. When you join the credit union, you become a shareholder – or an owner – of a very unique financial institution. Earnings above the required reserves are returned to you, in the form of lower interest rates on loans and competitive dividends on savings.">
+<meta name="keywords" content=""/>
+
+<?php include ('../includes/head.php'); ?>
+
+</head>
+
+<body>
+<?php include_once("../includes/analyticstracking.php") ?>
+<?php include ('../includes/header.php');?>
+
+<main id="main" class="content page">
+
+<div class="container">
+
+<div class="nine columns">
+<p class="page-title">Fee Schedule</p>
+
+
+<table id="fee-schedule-table" role="presentation">
+    
+    <tr>
+    <td> NSF Fee</td>
+    <td> $28.00/item</td>
+    </tr>
+    
+    <tr>
+    <td> Check Printing Fee
+(Depending on style)</td>
+    <td> Prices may vary </td>
+    </tr>
+    
+    <tr>
+    <td>Excessive Withdrawal Fee
+(If more than 6 per year and balance is under $250)</td>
+    <td>$5.00/withdrawal</td>
+    </tr>
+    
+    <tr>
+    <td>Check Copy Fee</td>
+    <td> $2.00/item</td>
+    </tr>
+    
+    <tr>
+    <td>Account Reconciliation and Research Fee</td>
+    <td>$20.00/hour plus copy fee</td>
+    </tr>
+    
+    <tr>
+    <td> Statement Copy Fee</td>
+    <td>$4.00/month</td>
+    </tr>
+    
+    <tr>
+    <td> Returned Deposit Item Fee</td>
+    <td>$25.00/item</td>
+    </tr>
+    
+    <tr>
+    <td> Account History</td>
+    <td> $1.00/week</td>
+    </tr>
+    
+    <tr>
+    <td> Collection Item</td>
+    <td>$10.00/item</td>
+    </tr>
+    
+    <tr>
+    <td> Wire – Outgoing</td>
+    <td>$15.00/transfer</td>
+    </tr>
+    
+    <tr>
+    <td>Wire – Incoming</td>
+    <td> FREE</td>
+    </tr>
+    
+    <tr>
+    <td>Wire – Foreign</td>
+    <td>$35.00/transfer</td>
+    </tr>
+    
+    <tr>
+    <td>Cashier's Check</td>
+    <td>$2.00/check</td>
+    </tr>
+    
+    <tr>
+    <td> Money Order</td>
+    <td>$2.00/money order</td>
+    </tr>
+    
+    <tr>
+    <td>ATM Withdrawals
+(In excess of 6 withdrawals per month or non-CGR Credit Union ATM machines)</td>
+    <td> $1.00/withdrawal</td>
+    </tr>
+    
+    <tr>
+    <td> Wrong Address Fee</td>
+    <td> $1.00</td>
+    </tr>
+    
+    <tr>
+    <td> Automatic Transfer Fee</td>
+    <td> $5.00/transfer</td>
+    </tr>
+    
+    <tr>
+    <td> Account/Deposit Verifications</td>
+    <td> $10.00/verification</td>
+    </tr>
+    
+    <tr>
+    <td>Inactive Account Fee
+(Maximum of 12 months; after 2 years of inactivity. Retroactive.)</td>
+    <td> $5.00/month</td>
+    </tr>
+    
+    <tr>
+    <td> Debit Card Replacement Fee</td>
+    <td> $5.00</td>
+    </tr>
+    
+    <tr>
+    <td> Return Guard</td>
+    <td> $28.00</td>
+    </tr>
+    
+    <tr>
+    <td> Stop Payment Fee</td>
+    <td> $28.00</td>
+    </tr>
+    
+    <tr>
+    <td>Check Cashing Fee
+(Check cashing fee charged only if member does not have an account balance to cover the amount of check or does not have direct deposit relationship with CGR)</td>
+    <td> $5.00</td>
+    </tr>
+    
+    <tr>
+    <td> Share Savings</td>
+    <td> $5.00 par share value</td>
+    </tr>
+    
+    <tr>
+    <td> Share Draft</td>
+    <td> No Fee</td>
+    </tr>
+    
+    <tr>
+    <td> Prime Time Share Draft</td>
+    <td> 5 free money orders or cashier's checks a month</td>
+    </tr>
+    
+    <tr>
+    <td> Capital Share</td>
+    <td> $5.00 per withdrawal if more than 3 per month</td>
+    </tr>
+    
+    <tr>
+    <td> Advantage Capital Share</td>
+    <td> No excessive withdrawal fee, 5 free money orders or cashier's checks a month</td>
+    </tr>
+    
+    <tr>
+    <td> Christmas Club</td>
+    <td> $5.00 per withdrawal</td>
+    </tr>
+    
+    <tr>
+    <td> Vacation Club</td>
+    <td> 2 free withdrawals per year, then $5.00 per withdrawal</td>
+    </tr>
+    
+    <tr>
+    <td> CU College Club</td>
+    <td> free electronic statements, no monthly service charge</td>
+    </tr>
+    
+    <tr>
+    <td> Kid's Club Account</td>
+    <td> $5.00 par share value</td>
+    </tr>
+      
+       <tr>
+    <td> ACH NSF Fee</td>
+    <td> $28 per item</td>
+    </tr>
+      
+       <tr>
+    <td> ATM Overdraft Fee</td>
+    <td> $28 per item</td>
+    </tr>
+      
+       <tr>
+    <td> Bill Pay Account</td>
+    <td> No monthly fee</td>
+    </tr>
+        
+    </table>
+    
+    
+    <div class="clearfix"></div>
+    
+     
+
+<p class="df-title fw700 aligncenter m30-t">TRUTH IN SAVINGS DISCLOSURE</p>
+
+<p>Except as specifically described, the following disclosures apply to all of the accounts. All accounts described in this Truth-in-Savings Disclosure are share accounts.</p>
+
+    <p><strong>Rate Information.</strong> The Annual Percentage Yield is a percentage rate that reflects the total amount of dividends to be paid on an account based on the dividend rate and the frequency of compounding for an annual period. Dividends are determined by the Credit Union Board of Directors and are subject to change as indicated in the Rate & Fee Schedule. The Dividend Rate and Annual Percentage Yield were the rates paid as of the last dividend period.</p>
+    
+    <p><strong>Dividend Period.</strong> The dividend period begins on the first calendar day and ends on the last calendar day of the dividend period.</p></p>
+    
+    <p><strong>Dividend Compounding and Crediting.</strong> The compounding and crediting frequency of dividends are stated in the Rate & Fee Schedule.</p>
+    
+    <p><strong>Balance Information.</strong> The minimum balance requirements applicable to each account are set forth in the Rate Schedule. Some accounts may have additional minimum opening deposit or already have on deposit at least the par value of one full share in a Regular Share Savings account. The par value amount is stated in the Fee Schedule. Some accounts may have additional minimum opening deposit requirements. For all accounts, dividends are calculated by the Average Daily Balance method. Dividends are calculated by applying a periodic rate to the Average Daily Balance in the account for the dividend period. The Average Daily Balance is calculated by adding the balance in the account for each day of the period and dividing that figure by the number of days in the period.</p></p>
+    
+    <p><strong>Accrual of Dividends.</strong> For all accounts, dividends will begin to accrue on noncash deposits (e.g. checks) on the business day you make the deposit to your account. If you close your account before accrued dividends are credited, accrued dividends will not be paid.</p>
+    
+    <p><strong>Transaction Limitations.</strong> For Share Savings, Capital Shares, Retirement, Christmas & Vacation Club Accounts, no more than six (6) preauthorized, automatic, telephone transfers, checks, drafts or ACH may be made to another account of yours or to a third party in any month. The Credit Union will not complete transfers above this limit. For Christmas & Vacation Club Accounts, you may make transfers or withdrawals in the first seven (7) days after the account is opened without restriction. For checking accounts, no account limitations apply. For Christmas Club Accounts the entire balance in the account is mailed to you by the 10th of November and the account remains open. You may be charged a withdrawal fee for each withdrawal from the account, after the first seven (7) days the account is opened.</p>
+
+    
+    
+    
+</div><!--end eight columns-->
+
+<div class="three columns sidebar">
+<?php include ('../includes/accounts-sidebar.php'); ?>
+</div><!--end four columns sidebar-->
+	</div>
+</main>
+
+
+
+
+
+
+<?php include ('../includes/footer.php');?>
+
+<!--JAVASCRIPT INCLUDES-->
+
+
+</body>
+
+</html>
