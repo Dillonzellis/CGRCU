625a7e193d44fe5a9eb8d3f1806cbc0836954ba5
Dillon Ellis <dellis@fcul.com>
2022-07-28 13:41:24 -0400

    original head file

---
+++ includes/head.php
@@ -0,0 +1,45 @@
+<!--CSS STYLING-->
+<link rel="stylesheet" href="https://www.cgrcu.org/css/style.css"/>
+<link rel="stylesheet" href="https://www.cgrcu.org/css/normalize.css">
+<link rel="stylesheet" href="https://www.cgrcu.org/css/skeleton.css">
+<link rel="stylesheet" type="text/css" href="https://www.cgrcu.org/css/fullwidth.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="https://www.cgrcu.org/rs-plugin/css/settings.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="https://www.cgrcu.org/remodal/remodal.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="https://www.cgrcu.org/remodal/remodal-default-theme.css" media="screen" />
+<link rel="stylesheet" href="https://www.cgrcu.org/css/jquery-ui.theme.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+
+<!--JS SCRIPTS-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script> 
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular-route.js"></script>
+<script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"></script>
+
+<!-- REVOLUTION BANNER CSS SETTINGS -->
+<script type="text/javascript" src="https://www.cgrcu.org/rs-plugin/js/jquery.themepunch.plugins.min.js"></script>
+<script type="text/javascript" src="https://www.cgrcu.org/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+
+
+<!-- OTHER JS CODE -->
+<script type="text/javascript" src="https://www.cgrcu.org/remodal/remodal.js"></script>
+<script type="text/javascript" src="https://www.cgrcu.org/js/custom.js"></script>
+<script type="text/javascript" src="https://www.cgrcu.org/js/functions.js"></script>
+
+<!-- JS includes //-->
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries //-->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+<script src='https://www.google.com/recaptcha/api.js'></script>
+
+<script type="text/javascript">
+            function postLink(link) {
+                var f = document.createElement("form");
+                f.method = 'post';
+                f.action = link;
+                document.body.appendChild(f);
+                f.submit();
+            }
+        </script>
