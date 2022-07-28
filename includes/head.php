71bff07dd038bf775d3437e1d1d4d2633f6c9751
Dillon Ellis <dellis@fcul.com>
2022-07-28 13:43:01 -0400

    include new stylesheet

--- includes/head.php
+++ includes/head.php
@@ -1,5 +1,5 @@
 <!--CSS STYLING-->
-<link rel="stylesheet" href="https://www.cgrcu.org/css/style.css"/>
+<link rel="stylesheet" href="https://www.cgrcu.org/css/style.css" />
 <link rel="stylesheet" href="https://www.cgrcu.org/css/normalize.css">
 <link rel="stylesheet" href="https://www.cgrcu.org/css/skeleton.css">
 <link rel="stylesheet" type="text/css" href="https://www.cgrcu.org/css/fullwidth.css" media="screen" />
@@ -8,9 +8,10 @@
 <link rel="stylesheet" type="text/css" href="https://www.cgrcu.org/remodal/remodal-default-theme.css" media="screen" />
 <link rel="stylesheet" href="https://www.cgrcu.org/css/jquery-ui.theme.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+<link rel="stylesheet" href="../css/style-de.css">
 
 <!--JS SCRIPTS-->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular-route.js"></script>
 <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"></script>
@@ -35,11 +36,11 @@
 <script src='https://www.google.com/recaptcha/api.js'></script>
 
 <script type="text/javascript">
-            function postLink(link) {
-                var f = document.createElement("form");
-                f.method = 'post';
-                f.action = link;
-                document.body.appendChild(f);
-                f.submit();
-            }
-        </script>
+    function postLink(link) {
+        var f = document.createElement("form");
+        f.method = 'post';
+        f.action = link;
+        document.body.appendChild(f);
+        f.submit();
+    }
+</script>
