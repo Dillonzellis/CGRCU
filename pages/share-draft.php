7a67ad4cef8310795aec83e6d983a424f2379e1b
Dillon Ellis <dellis@fcul.com>
2022-08-08 16:42:24 -0400

    fix button bug and iframe margins

--- pages/share-draft.php
+++ pages/share-draft.php
@@ -2,188 +2,190 @@
 <html lang="en">
 
 <head>
-<title>CGR Credit Union - Checking Accounts</title>
+  <title>CGR Credit Union - Checking Accounts</title>
 
-<!--META DATA-->
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1"/>
-<meta name="description" content="Like all credit unions, CGR Credit Union is a not-for-profit financial cooperative. When you join the credit union, you become a shareholder – or an owner – of a very unique financial institution. Earnings above the required reserves are returned to you, in the form of lower interest rates on loans and competitive dividends on savings.">
-<meta name="keywords" content=""/>
+  <!--META DATA-->
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="description" content="Like all credit unions, CGR Credit Union is a not-for-profit financial cooperative. When you join the credit union, you become a shareholder – or an owner – of a very unique financial institution. Earnings above the required reserves are returned to you, in the form of lower interest rates on loans and competitive dividends on savings.">
+  <meta name="keywords" content="" />
 
-<?php include ('../includes/head.php'); ?>
+  <?php include('../includes/head.php'); ?>
 
 </head>
 
 <body>
-<?php include_once("../includes/analyticstracking.php") ?>
-<?php include ('../includes/header.php');?>
+  <?php include_once("../includes/analyticstracking.php") ?>
+  <?php include('../includes/header.php'); ?>
 
-<main id="main" class="content page">
+  <main id="main" class="content page">
 
-<div class="container">
+    <div class="container">
 
-<div class="nine columns">
- 	
- 
-<h1 class="page-title">Checking Accounts</h1>
+      <div class="nine columns">
 
 
-<p class="df-mtitle">At CGR Credit Union, we have a checking account to meet your needs. </p>
-	
-	<p class="df-mtitle fw600">Introducing our BRAND NEW Checking Account Line Up:</p>
-
-	<style>
-		.alternate tr:nth-child(2n) {
-			background: #ddd;
-		}
-	</style>
-
-<table width="100%" border="1" align="center" class="alternate" role="presentation">
-<tr>
-<td width="252">&nbsp;</td>
-<td width="15%" align="center"><strong>Premier</strong></td>
-<td width="15%" align="center"><strong>Preferred</strong></td>
-<td width="15%" align="center"><strong>Classic</strong></td>
-<td width="15%" align="center"><strong>Value</strong></td>
-</tr>
-
-<tr>
-<td>Monthly Fees</td>
-<td align="center">$5</td>
-<td align="center">$2</td>
-<td align="center">$0</td>
-<td align="center">$7</td>
-</tr>
-
-<tr>
-<td>Minimum Balance</td>
-<td align="center">$0</td>
-<td align="center">$0</td>
-<td align="center">$0</td>
-<td align="center">$0</td>
-</tr>
-
-<tr>
-<td>Earns Interest with Listed Minimum Balance</td>
-<td align="center">$5,000/$10,000</td>
-<td align="center">$5,000/$10,000</td>
-<td align="center">-</td>
-<td align="center">-</td>
-</tr>
-<tr>
-  <td>Overdraft Privilege</td>
- <td align="center">✓</td>
-  <td align="center">✓</td>
-  <td align="center">✓</td>
-  <td align="center">-</td>
-</tr>
-<tr>
-  <td>Bill Pay</td>
-  <td align="center">✓</td>
-  <td align="center">✓</td>
-  <td align="center">✓</td>
- <td align="center">✓</td>
-</tr>
-<tr>
-  <td>Fully Managed Identity Theft Recovery - three generations</td>
- <td align="center">✓</td>
-  <td align="center">✓</td>
-  <td align="center">-</td>
-  <td align="center">✓</td>
-</tr>
-
-<tr>
-  <td>Identity Theft Reimbursement Insurance - $25K</td>
-  <td align="center">✓</td>
-  <td align="center">-</td>
-  <td align="center">-</td>
-  <td align="center">-</td>
-</tr>
-<tr>
-  <td>Credit Monitoring (TU)</td>
-  <td align="center">✓</td>
-  <td align="center">-</td>
-  <td align="center">-</td>
-  <td align="center">-</td>
-</tr>
-<tr>
-  <td>Monthly Credit Score</td>
-  <td align="center">✓</td>
-  <td align="center">-</td>
-  <td align="center">-</td>
-  <td align="center">-</td>
-</tr>
-
-<tr>
-  <td>Dark Web Monitoring - profile, email, internet surveillance</td>
-  <td align="center">✓</td>
- <td align="center">-</td>
-  <td align="center">-</td>
- <td align="center">-</td>
-</tr>
-<tr>
-  <td>Cell Phone Protection - up to $500 per claim/$50 deductible /2 claims per year</td>
-  <td align="center">✓</td>
-  <td align="center">-</td>
- <td align="center">-</td>
-  <td align="center">-</td>
-</tr>
-<tr>
-  <td>Consumer Loan Discount .25%</td>
-  <td align="center">✓</td>
-  <td align="center">-</td>
-  <td align="center">-</td>
-  <td align="center">-</td>
-</tr>
-
-</table><br>
-
-<p><strong>These checking features apply to all of our Checking Accounts:</strong></p>
-<ul>
-  <li>Access to a shared network of No-fee credit union ATMs nationwide</li>
-  <li>FREE Internet Banking access</li>
-  <li>FREE Statements Online (if elected)</li>
-  <li>FREE eAlerts</li>
-  <li>FREE <a href="https://www.cgrcu.org/pages/mobile-banking.php">Mobile Banking</a></li>
-  <li>FREE Account access through <a href="https://www.cgrcu.org/pages/timely-teller.php">Timely Teller</a>, our 24-hour telephone response system</li>
-	<li>FREE <a href="https://www.cgrcu.org/pages/debit-check.php">VISA® Debit Card</a> (1st card &amp; renewals)</li>
-  <li>Pre-authorized automatic drafts (no charge)</li>
-  <li>Overdraft transfer protection with savings or a pre-approved line of credit</li>
-	<li><a href="https://www.cgrcu.org/pages/direct-deposit.php">Direct deposit</a></li>
-  <li>Wire transfer of funds to an outside account (fee applies)</li>
-  <li>Stop payments (fee applies)</li>
-  <li>Remote Deposit</li>
-</ul>
-  <p> *Dividends are compounded and paid monthly.<br> 	 
-    Note: Other terms and conditions apply.  Some features require activation.</p> 
-    <p> For additional details about identity theft recovery and monitoring services and mobile phone coverage included with Premier Checking, including terms, conditions, and limitations, please visit <a href="#" data-remodal-target="nxg">www.cgrcu.nxgstrategies.com</a>.</p>
-	<p>For more information about the new identity theft recovery benefit included with Preferred and Value Checking, visit <a href="#" data-remodal-target="nxgCares">cgrcucares.nxgstrategies.com</a></p>
-	
-
-    <button class="bold-btn"><a href="https://www.secure-cgrcu.org/Centryx/servlet/com.sos.webteller.onlineforms.AcctPassApp" target="_blank">Open a checking account</a></button>
-    
-	<p align="left"><iframe width="560" height="315" src="https://www.youtube.com/embed/wUqzukm0NpI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="Remote Access to CGR CU"></iframe></p>
-<div class="clearfix m20-t"></div>
+        <h1 class="page-title">Checking Accounts</h1>
 
 
+        <p class="df-mtitle">At CGR Credit Union, we have a checking account to meet your needs. </p>
+
+        <p class="df-mtitle fw600">Introducing our BRAND NEW Checking Account Line Up:</p>
+
+        <style>
+          .alternate tr:nth-child(2n) {
+            background: #ddd;
+          }
+        </style>
+
+        <table width="100%" border="1" align="center" class="alternate" role="presentation">
+          <tr>
+            <td width="252">&nbsp;</td>
+            <td width="15%" align="center"><strong>Premier</strong></td>
+            <td width="15%" align="center"><strong>Preferred</strong></td>
+            <td width="15%" align="center"><strong>Classic</strong></td>
+            <td width="15%" align="center"><strong>Value</strong></td>
+          </tr>
+
+          <tr>
+            <td>Monthly Fees</td>
+            <td align="center">$5</td>
+            <td align="center">$2</td>
+            <td align="center">$0</td>
+            <td align="center">$7</td>
+          </tr>
+
+          <tr>
+            <td>Minimum Balance</td>
+            <td align="center">$0</td>
+            <td align="center">$0</td>
+            <td align="center">$0</td>
+            <td align="center">$0</td>
+          </tr>
+
+          <tr>
+            <td>Earns Interest with Listed Minimum Balance</td>
+            <td align="center">$5,000/$10,000</td>
+            <td align="center">$5,000/$10,000</td>
+            <td align="center">-</td>
+            <td align="center">-</td>
+          </tr>
+          <tr>
+            <td>Overdraft Privilege</td>
+            <td align="center">✓</td>
+            <td align="center">✓</td>
+            <td align="center">✓</td>
+            <td align="center">-</td>
+          </tr>
+          <tr>
+            <td>Bill Pay</td>
+            <td align="center">✓</td>
+            <td align="center">✓</td>
+            <td align="center">✓</td>
+            <td align="center">✓</td>
+          </tr>
+          <tr>
+            <td>Fully Managed Identity Theft Recovery - three generations</td>
+            <td align="center">✓</td>
+            <td align="center">✓</td>
+            <td align="center">-</td>
+            <td align="center">✓</td>
+          </tr>
+
+          <tr>
+            <td>Identity Theft Reimbursement Insurance - $25K</td>
+            <td align="center">✓</td>
+            <td align="center">-</td>
+            <td align="center">-</td>
+            <td align="center">-</td>
+          </tr>
+          <tr>
+            <td>Credit Monitoring (TU)</td>
+            <td align="center">✓</td>
+            <td align="center">-</td>
+            <td align="center">-</td>
+            <td align="center">-</td>
+          </tr>
+          <tr>
+            <td>Monthly Credit Score</td>
+            <td align="center">✓</td>
+            <td align="center">-</td>
+            <td align="center">-</td>
+            <td align="center">-</td>
+          </tr>
+
+          <tr>
+            <td>Dark Web Monitoring - profile, email, internet surveillance</td>
+            <td align="center">✓</td>
+            <td align="center">-</td>
+            <td align="center">-</td>
+            <td align="center">-</td>
+          </tr>
+          <tr>
+            <td>Cell Phone Protection - up to $500 per claim/$50 deductible /2 claims per year</td>
+            <td align="center">✓</td>
+            <td align="center">-</td>
+            <td align="center">-</td>
+            <td align="center">-</td>
+          </tr>
+          <tr>
+            <td>Consumer Loan Discount .25%</td>
+            <td align="center">✓</td>
+            <td align="center">-</td>
+            <td align="center">-</td>
+            <td align="center">-</td>
+          </tr>
+
+        </table><br>
+
+        <p><strong>These checking features apply to all of our Checking Accounts:</strong></p>
+        <ul>
+          <li>Access to a shared network of No-fee credit union ATMs nationwide</li>
+          <li>FREE Internet Banking access</li>
+          <li>FREE Statements Online (if elected)</li>
+          <li>FREE eAlerts</li>
+          <li>FREE <a href="https://www.cgrcu.org/pages/mobile-banking.php">Mobile Banking</a></li>
+          <li>FREE Account access through <a href="https://www.cgrcu.org/pages/timely-teller.php">Timely Teller</a>, our 24-hour telephone response system</li>
+          <li>FREE <a href="https://www.cgrcu.org/pages/debit-check.php">VISA® Debit Card</a> (1st card &amp; renewals)</li>
+          <li>Pre-authorized automatic drafts (no charge)</li>
+          <li>Overdraft transfer protection with savings or a pre-approved line of credit</li>
+          <li><a href="https://www.cgrcu.org/pages/direct-deposit.php">Direct deposit</a></li>
+          <li>Wire transfer of funds to an outside account (fee applies)</li>
+          <li>Stop payments (fee applies)</li>
+          <li>Remote Deposit</li>
+        </ul>
+        <p> *Dividends are compounded and paid monthly.<br>
+          Note: Other terms and conditions apply. Some features require activation.</p>
+        <p> For additional details about identity theft recovery and monitoring services and mobile phone coverage included with Premier Checking, including terms, conditions, and limitations, please visit <a href="#" data-remodal-target="nxg">www.cgrcu.nxgstrategies.com</a>.</p>
+        <p>For more information about the new identity theft recovery benefit included with Preferred and Value Checking, visit <a href="#" data-remodal-target="nxgCares">cgrcucares.nxgstrategies.com</a></p>
 
 
-</div><!--end eight columns-->
+        <a class="bold-btn" href="https://www.secure-cgrcu.org/Centryx/servlet/com.sos.webteller.onlineforms.AcctPassApp" target="_blank">Open a checking account</a>
 
-<div class="three columns sidebar">
-<?php include ('../includes/accounts-sidebar.php'); ?>
-</div><!--end four columns sidebar-->
-	</div>
-</main>
+        <p align="left" style="margin-top: 20px"><iframe width="560" height="315" src="https://www.youtube.com/embed/wUqzukm0NpI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="Remote Access to CGR CU"></iframe></p>
+        <div class="clearfix m20-t"></div>
 
 
 
 
+      </div>
+      <!--end eight columns-->
+
+      <div class="three columns sidebar">
+        <?php include('../includes/accounts-sidebar.php'); ?>
+      </div>
+      <!--end four columns sidebar-->
+    </div>
+  </main>
 
 
-<?php include ('../includes/footer.php');?>
 
-<!--JAVASCRIPT INCLUDES-->
+
+
+
+  <?php include('../includes/footer.php'); ?>
+
+  <!--JAVASCRIPT INCLUDES-->
 
 
 </body>
