840a20a83a8b1db7152d1467d0ceea76a83c0f45
Dillon Ellis <dellis@fcul.com>
2022-08-11 16:19:24 -0400

    change effective date to the 1st

--- pages/share-rates.php
+++ pages/share-rates.php
@@ -2,242 +2,245 @@
 <html lang="en">
 
 <head>
-<title>CGR Credit Union - Share Rates</title>
+    <title>CGR Credit Union - Share Rates</title>
 
-<!--META DATA-->
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1"/>
-<meta name="description" content="Like all credit unions, CGR Credit Union is a not-for-profit financial cooperative. When you join the credit union, you become a shareholder – or an owner – of a very unique financial institution. Earnings above the required reserves are returned to you, in the form of lower interest rates on loans and competitive dividends on savings.">
-<meta name="keywords" content=""/>
+    <!--META DATA-->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Like all credit unions, CGR Credit Union is a not-for-profit financial cooperative. When you join the credit union, you become a shareholder – or an owner – of a very unique financial institution. Earnings above the required reserves are returned to you, in the form of lower interest rates on loans and competitive dividends on savings.">
+    <meta name="keywords" content="" />
 
-<?php include ('../includes/head.php'); ?>
+    <?php include('../includes/head.php'); ?>
 
 </head>
 
 <body>
-<?php include_once("../includes/analyticstracking.php") ?>
-<?php include ('../includes/header.php');?>
+    <?php include_once("../includes/analyticstracking.php") ?>
+    <?php include('../includes/header.php'); ?>
 
-<main id="main" class="content page">
+    <main id="main" class="content page">
 
-<div class="container">
+        <div class="container">
 
-<div class="nine columns">
-<h1 class="page-title">Share Rates</h1>
+            <div class="nine columns">
+                <h1 class="page-title">Share Rates</h1>
 
-<p>Annual Percentage Yield is accurate as of April 6, 2020. Rates and other terms may change at any time without prior notice. A penalty is imposed for early withdrawal. Fees could reduce earnings on the account.</p>
+                <p>Annual Percentage Yield is accurate as of April 1, 2020. Rates and other terms may change at any time without prior notice. A penalty is imposed for early withdrawal. Fees could reduce earnings on the account.</p>
 
-<table class="rates-table">
+                <table class="rates-table">
 
-<thead>
-	<tr>
-<th>Account Type</th>
-<th>Dividend Rate</th>
-<th>Annual Percentage Yield (APY)</th>
-<th>Minimum Balance to Earn Dividend</th>
-	</tr>
-</thead>
-<tbody>
-<tr>
-<td>
-<strong>Capital Share (Money Market)</strong><br/>
-  	$500 - $2,499<br/>
-	$2,500 and up</td>
-<td>
-0.15%<br/>
-0.15%
-</td>
-<td>
-0.15%<br/>
-0.15%
-</td>
-<td>
-$500/$2,500<br/>
-Tiered Pricing
-</td>
-</tr>
+                    <thead>
+                        <tr>
+                            <th>Account Type</th>
+                            <th>Dividend Rate</th>
+                            <th>Annual Percentage Yield (APY)</th>
+                            <th>Minimum Balance to Earn Dividend</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <strong>Capital Share (Money Market)</strong><br />
+                                $500 - $2,499<br />
+                                $2,500 and up
+                            </td>
+                            <td>
+                                0.15%<br />
+                                0.15%
+                            </td>
+                            <td>
+                                0.15%<br />
+                                0.15%
+                            </td>
+                            <td>
+                                $500/$2,500<br />
+                                Tiered Pricing
+                            </td>
+                        </tr>
 
-<tr>
-  <td><strong>Individual Retirement Account</strong></td>
-  <td>0.30%</td>
-  <td>0.30%</td>
-  <td>$100</td>
-</tr>
+                        <tr>
+                            <td><strong>Individual Retirement Account</strong></td>
+                            <td>0.30%</td>
+                            <td>0.30%</td>
+                            <td>$100</td>
+                        </tr>
 
-<tr>
-<td><strong>IRA Advantage</strong></td>
-<td>0.35%</td>
-<td>0.35%</td>
-<td>$5,000</td>
-</tr>
+                        <tr>
+                            <td><strong>IRA Advantage</strong></td>
+                            <td>0.35%</td>
+                            <td>0.35%</td>
+                            <td>$5,000</td>
+                        </tr>
 
-<tr>
-<td><strong>Premier Checking</strong><br/>$5,000 - 9,999.99<br>
-$10,000 & Greater</td>
-<td><br>
-  0.05%<br>
-  0.10%</td>
-<td><br>
-  0.05%<br>
-  0.10%</td>
-<td>$5,000</td>
-</tr>
-	
-<tr>
-<td><strong>Preferred Checking</strong><br/>$5,000 - 9,999.99<br>
-$10,000 & Greater</td>
-<td><br>
-  0.05%<br>
-  0.10%</td>
-<td><br>
-  0.05%<br>
-  0.10%</td>
-<td>$5,000</td>
-</tr>
-	
-<tr>
-<td><strong>Regular Shares</strong><br/>
-$100 &amp; Greater</td>
-<td>0.05%</td>
-<td>0.05%</td>
-<td>$100</td>
-</tr>
+                        <tr>
+                            <td><strong>Premier Checking</strong><br />$5,000 - 9,999.99<br>
+                                $10,000 & Greater</td>
+                            <td><br>
+                                0.05%<br>
+                                0.10%</td>
+                            <td><br>
+                                0.05%<br>
+                                0.10%</td>
+                            <td>$5,000</td>
+                        </tr>
 
-<tr>
-<td>
-<strong>Kid's Club</strong><br/>
-  	$5 - Up<br/>
-	$250 - 12 Month CD
-    </td>
-<td>
-0.15%<br/>
-0.25%
-</td>
-<td>
-0.15%<br/>
-0.25%
-</td>
-<td>
-$5<br/>
-$250
-</td>
-</tr>
+                        <tr>
+                            <td><strong>Preferred Checking</strong><br />$5,000 - 9,999.99<br>
+                                $10,000 & Greater</td>
+                            <td><br>
+                                0.05%<br>
+                                0.10%</td>
+                            <td><br>
+                                0.05%<br>
+                                0.10%</td>
+                            <td>$5,000</td>
+                        </tr>
 
-<tr>
-<td>Christmas Club</td>
-<td>0.15%</td>
-<td>0.15%</td>
-<td>$100</td>
-</tr>
+                        <tr>
+                            <td><strong>Regular Shares</strong><br />
+                                $100 &amp; Greater</td>
+                            <td>0.05%</td>
+                            <td>0.05%</td>
+                            <td>$100</td>
+                        </tr>
 
-<tr>
-<td>Vacation Club</td>
-<td>0.15%</td>
-<td>0.15%</td>
-<td>$100</td>
-</tr>
+                        <tr>
+                            <td>
+                                <strong>Kid's Club</strong><br />
+                                $5 - Up<br />
+                                $250 - 12 Month CD
+                            </td>
+                            <td>
+                                0.15%<br />
+                                0.25%
+                            </td>
+                            <td>
+                                0.15%<br />
+                                0.25%
+                            </td>
+                            <td>
+                                $5<br />
+                                $250
+                            </td>
+                        </tr>
 
-	</tbody>
-</table>
-<div class="clearfix m20-t"></div>
+                        <tr>
+                            <td>Christmas Club</td>
+                            <td>0.15%</td>
+                            <td>0.15%</td>
+                            <td>$100</td>
+                        </tr>
 
-<p class="df-mtitle gbd_red"><strong>Share Certificate</strong></p>
+                        <tr>
+                            <td>Vacation Club</td>
+                            <td>0.15%</td>
+                            <td>0.15%</td>
+                            <td>$100</td>
+                        </tr>
 
-<p>Annual Percentage Yield is accurate as of September 30, 2020. Rates and other terms may change at any time without prior notice. After your certificate is opened, the interest is fixed for the term. A penalty is imposed for early withdrawal. Fees could reduce earnings on the account.</p>
-<ul>
-    <li>Interest compounded monthly.</li>
-    <li>If you close your share account before interest is paid, you will not receive the accrued interest.</li>
-    </ul>
+                    </tbody>
+                </table>
+                <div class="clearfix m20-t"></div>
 
-<table class="rates-table">
+                <p class="df-mtitle gbd_red"><strong>Share Certificate</strong></p>
 
-<thead>
-	<tr>
-<th>Account Type</th>
-<th>Interest Rate</th>
-<th>Annual Percentage Yield (APY)</th>
-<th>Minimum Balance to Earn Interest</th>
-	</tr>
-</thead>
-<tbody>
-	
-<tr>
-  <td><strong>6 Month</strong></td>
-  <td>0.50%</td>
-  <td>0.50%</td>
-  <td>$2,500</td>
-</tr>
+                <p>Annual Percentage Yield is accurate as of September 30, 2020. Rates and other terms may change at any time without prior notice. After your certificate is opened, the interest is fixed for the term. A penalty is imposed for early withdrawal. Fees could reduce earnings on the account.</p>
+                <ul>
+                    <li>Interest compounded monthly.</li>
+                    <li>If you close your share account before interest is paid, you will not receive the accrued interest.</li>
+                </ul>
 
-<tr>
-<td><strong>12 Month</strong></td>
-<td>.60%</td>
-<td>.60%</td>
-<td>$2,500</td>
-</tr>
-	
-<tr>
-<td><strong>NEW 18 Month</strong></td>
-<td>.60%</td>
-<td>.60%</td>
-<td>$2,500</td>
-</tr>
+                <table class="rates-table">
 
-<tr>
-  <td><strong>24 Month</strong></td>
-  <td>.70%</td>
-  <td>.70%</td>
-  <td>$2,500</td>
-</tr>
+                    <thead>
+                        <tr>
+                            <th>Account Type</th>
+                            <th>Interest Rate</th>
+                            <th>Annual Percentage Yield (APY)</th>
+                            <th>Minimum Balance to Earn Interest</th>
+                        </tr>
+                    </thead>
+                    <tbody>
 
-<tr>
-<td><strong>36 Month</strong></td>
-<td>.80%</td>
-<td>.80%</td>
-<td>$2,500</td>
-</tr>
+                        <tr>
+                            <td><strong>6 Month</strong></td>
+                            <td>0.50%</td>
+                            <td>0.50%</td>
+                            <td>$2,500</td>
+                        </tr>
 
-<tr>
-<td><strong>48 Month</strong></td>
-<td>0.80%</td>
-<td>0.80%</td>
-<td>$2,500</td>
-</tr>
+                        <tr>
+                            <td><strong>12 Month</strong></td>
+                            <td>.60%</td>
+                            <td>.60%</td>
+                            <td>$2,500</td>
+                        </tr>
 
+                        <tr>
+                            <td><strong>NEW 18 Month</strong></td>
+                            <td>.60%</td>
+                            <td>.60%</td>
+                            <td>$2,500</td>
+                        </tr>
 
-<tr>
-<td><strong>60 Month</strong></td>
-<td>0.90%</td>
-<td>0.90%</td>
-<td>$2,500</td>
-</tr>
+                        <tr>
+                            <td><strong>24 Month</strong></td>
+                            <td>.70%</td>
+                            <td>.70%</td>
+                            <td>$2,500</td>
+                        </tr>
+
+                        <tr>
+                            <td><strong>36 Month</strong></td>
+                            <td>.80%</td>
+                            <td>.80%</td>
+                            <td>$2,500</td>
+                        </tr>
+
+                        <tr>
+                            <td><strong>48 Month</strong></td>
+                            <td>0.80%</td>
+                            <td>0.80%</td>
+                            <td>$2,500</td>
+                        </tr>
 
 
-	</tbody>
-</table>
-
-<div class="clearfix m20-t"></div>
-
-
-<p>*Rates and Terms are subject to change without notice. Rates shown are effective as of  9/30/2020</p>
-
-<p> For additional information regarding the above accounts, please refer to the <a href="fee-schedule.php" aria-label="Truth-In-Savings Schedule">Truth-In-Savings Schedule</a>.</p>
-
-</div><!--end eight columns-->
-
-<div class="three columns sidebar">
-<?php include ('../includes/accounts-sidebar.php'); ?>
-</div><!--end four columns sidebar-->
-	</div>
-</main>
+                        <tr>
+                            <td><strong>60 Month</strong></td>
+                            <td>0.90%</td>
+                            <td>0.90%</td>
+                            <td>$2,500</td>
+                        </tr>
 
 
+                    </tbody>
+                </table>
+
+                <div class="clearfix m20-t"></div>
+
+
+                <p>*Rates and Terms are subject to change without notice. Rates shown are effective as of 9/30/2020</p>
+
+                <p> For additional information regarding the above accounts, please refer to the <a href="fee-schedule.php" aria-label="Truth-In-Savings Schedule">Truth-In-Savings Schedule</a>.</p>
+
+            </div>
+            <!--end eight columns-->
+
+            <div class="three columns sidebar">
+                <?php include('../includes/accounts-sidebar.php'); ?>
+            </div>
+            <!--end four columns sidebar-->
+        </div>
+    </main>
 
 
 
 
-<?php include ('../includes/footer.php');?>
 
-<!--JAVASCRIPT INCLUDES-->
+
+    <?php include('../includes/footer.php'); ?>
+
+    <!--JAVASCRIPT INCLUDES-->
 
 
 </body>
