76423c490b66334a49d59070766140dc079c4435
Dillon Ellis <dellis@fcul.com>
2022-08-11 16:35:07 -0400

    remove identity theft from services sidebar include

--- includes/services-sidebar.php
+++ includes/services-sidebar.php
@@ -1,12 +1,11 @@
 <aside class="default-sidebar services-sidebar">
-<ul>
-<li><a href="https://www.cgrcu.org/pages/debit-check.php">Debit/Check Card Services</a></li>
-	<li><a href="https://www.cgrcu.org/pages/assistance-visa-credit.php">Debit/Check Card Assistance</a></li>
-<li><a href="https://www.cgrcu.org/pages/timely-teller.php">Timely Teller/24 Hour Audio Response</a></li>
-<li><a href="https://www.cgrcu.org/pages/identity-theft.php">Identity Theft Information</a></li>
-	<li><a href="https://www.cgrcu.org/pages/news.php">News & Resources</a></li>
-<li><a href="#" data-remodal-target="trustage-auto">TruStage Auto & Home Insurance Program</a></li>
-<li><a href="#" data-remodal-target="trustage-accidental">TruStage Accidental Death & Dismemberment Coverage</a></li>
-	<li><a href="https://www.cgrcu.org/pages/travel-club.php">Credit Union Travel Club</a></li>
-	</ul>
+    <ul>
+        <li><a href="https://www.cgrcu.org/pages/debit-check.php">Debit/Check Card Services</a></li>
+        <li><a href="https://www.cgrcu.org/pages/assistance-visa-credit.php">Debit/Check Card Assistance</a></li>
+        <li><a href="https://www.cgrcu.org/pages/timely-teller.php">Timely Teller/24 Hour Audio Response</a></li>
+        <li><a href="https://www.cgrcu.org/pages/news.php">News & Resources</a></li>
+        <li><a href="#" data-remodal-target="trustage-auto">TruStage Auto & Home Insurance Program</a></li>
+        <li><a href="#" data-remodal-target="trustage-accidental">TruStage Accidental Death & Dismemberment Coverage</a></li>
+        <li><a href="https://www.cgrcu.org/pages/travel-club.php">Credit Union Travel Club</a></li>
+    </ul>
 </aside>
