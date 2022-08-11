d7c04f7ee25e545b7e40fb856a46d3a47b47f7bc
Dillon Ellis <dellis@fcul.com>
2022-08-11 16:31:48 -0400

    change hyperlink text to correct destination

--- pages/overdraft-protection.php
+++ pages/overdraft-protection.php
@@ -2,152 +2,154 @@
 <html lang="en">
 
 <head>
-<title>CGR Credit Union - Overdraft Protection</title>
+    <title>CGR Credit Union - Overdraft Protection</title>
 
-<!--META DATA-->
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1"/>
-<meta name="description" content="Like all credit unions, CGR Credit Union is a not-for-profit financial cooperative. When you join the credit union, you become a shareholder – or an owner – of a very unique financial institution. Earnings above the required reserves are returned to you, in the form of lower interest rates on loans and competitive dividends on savings.">
-<meta name="keywords" content=""/>
+    <!--META DATA-->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Like all credit unions, CGR Credit Union is a not-for-profit financial cooperative. When you join the credit union, you become a shareholder – or an owner – of a very unique financial institution. Earnings above the required reserves are returned to you, in the form of lower interest rates on loans and competitive dividends on savings.">
+    <meta name="keywords" content="" />
 
-<?php include ('../includes/head.php'); ?>
+    <?php include('../includes/head.php'); ?>
 
 </head>
 
 <body>
-<?php include_once("../includes/analyticstracking.php") ?>
-<?php include ('../includes/header.php');?>
+    <?php include_once("../includes/analyticstracking.php") ?>
+    <?php include('../includes/header.php'); ?>
 
-<main id="main" class="content page">
+    <main id="main" class="content page">
 
-<div class="container">
+        <div class="container">
 
-<div class="nine columns">
- 	
-<div class="twelve columns">
+            <div class="nine columns">
 
-<div class="eight columns"> 
-<h1 class="page-title">Overdraft Protection</h1>
+                <div class="twelve columns">
 
-<p>Life happens!  CGR Credit Union understands that unexpected overdrafts occur from time to time.  When you need it, overdraft protection can help.</p>
+                    <div class="eight columns">
+                        <h1 class="page-title">Overdraft Protection</h1>
 
-<p class="df-stitle fw500">Overdraft Coverage Options</p>
+                        <p>Life happens! CGR Credit Union understands that unexpected overdrafts occur from time to time. When you need it, overdraft protection can help.</p>
 
-<p>CGR offers two options for overdraft protection.  The choice is yours which coverage you want.</p>
-</div>
+                        <p class="df-stitle fw500">Overdraft Coverage Options</p>
 
-<div class="three columns">
-<p><img src="../images/pages/overdraft.jpg" alt="Overdraft Protection" title="Overdraft Protection"></p>
-</div>
+                        <p>CGR offers two options for overdraft protection. The choice is yours which coverage you want.</p>
+                    </div>
 
-</div>
+                    <div class="three columns">
+                        <p><img src="../images/pages/overdraft.jpg" alt="Overdraft Protection" title="Overdraft Protection"></p>
+                    </div>
 
-
-<table class="overdraft-table" role="presentation">
-<thead>
-<td>Service</td>
-<td>Cost</td>
-</thead>
-
-<tr>
-<td>Overdraft Protection Link to Savings Account</td>
-<td>$5.00 fee per transfer</td>
-</tr>
-
-<tr>
-<td>Overdraft Protection ReturnGuard (Up to $300)</td>
-<td>$28 overdraft fee per item</td>
-</tr>
-
-</table>
-	<p>The Credit Union's fee of $28 per item will be deducted from the Return Guard limit.</p>
-	<p>We pay overdrafts at our discretion, which means <strong>we do not guarantee</strong> that we will always authorize and pay any type of transaction. If we <strong>do not</strong> authorize and pay an overdraft, your transaction will be declined.</p>
-	<p>Call us at (478) 745 – 0494 or come by a branch today to learn more about these services.</p>
-
-	<p>Ready to sign up? To opt in or out of extended coverage with your debit card, please <a href="mailto:msoq@cgrcu.org" title="Contact a Member Service Rep">contact</a> a member service representative.</p>
-
-<table class="overdraft-table" role="presentation">
-<thead>
-<td>Transactions Covered with Overdraft Privilege</td>
-<td>Standard Coverage (No action required)</td>
-<td>Extended Coverage (Your consent required)*</td>
-<td class="no-border">If you would like to select Extended Coverage for future transactions:</td>
-</thead>
-
-<tr>
-<td>Checks</td>
-<td>X</td>
-<td>X</td>
-<td class="no-border">Call us at: (478) 745 - 0494</td>
-</tr>
-
-<tr>
-<td>ACH – Automatic Debits</td>
-<td>X</td>
-<td>X</td>
-	<td class="no-border"><a href="mailto:msoq@cgrcu.org" title="Contact a Member Service Rep">Contact</a> a member service representative.</td>
-</tr>
-
-<tr>
-<td>Recurring Debit Card Payments</td>
-<td>X</td>
-<td>X</td>
-<td class="no-border"><a href="https://www.cgrcu.org/pages/locations.php">Visit any branch</a></td>
-</tr>
-
-<tr>
-<td>Online Bill Pay Items</td>
-<td>X</td>
-<td>X</td>
-<td class="no-border"></td>
-</tr>
-
-<tr>
-<td>Online Banking Transfers</td>
-<td>X</td>
-<td>X</td>
-<td class="no-border"></td>
-</tr>
-
-<tr>
-<td>ATM Withdrawals</td>
-<td></td>
-<td>X*</td>
-<td class="no-border"></td>
-</tr>
-
-<tr>
-<td>Everyday Debit Card Transactions</td>
-<td></td>
-<td>X*</td>
-<td class="no-border"></td>
-</tr>
-
-</table>
-
-<div class="clearfix"></div>
-
-<p>You can discontinue the Overdraft Protection at any time by contacting us at (478) 745 – 0494.</p> 
-
-<p><a href="https://www.cgrcu.org/pages/contact-us.php">Report Lost or Stolen Card</a></p>
-<p>The consumer must repay any overdraft on his or her account within 30 days.&nbsp; If the account remains negative after 30 days, account may be closed at the Credit Union's discretion.
-  CGR Credit Union will cover overdrafts on eligible accounts at their discretion up to $329.95; any amount over the limit will not be covered by the credit union.</p>
-</div><!--end eight columns-->
-
-<div class="three columns sidebar">
-<?php include ('../includes/accounts-sidebar.php'); ?>
-</div><!--end four columns sidebar-->
-	</div>
-</main>
+                </div>
 
 
+                <table class="overdraft-table" role="presentation">
+                    <thead>
+                        <td>Service</td>
+                        <td>Cost</td>
+                    </thead>
+
+                    <tr>
+                        <td>Overdraft Protection Link to Savings Account</td>
+                        <td>$5.00 fee per transfer</td>
+                    </tr>
+
+                    <tr>
+                        <td>Overdraft Protection ReturnGuard (Up to $300)</td>
+                        <td>$28 overdraft fee per item</td>
+                    </tr>
+
+                </table>
+                <p>The Credit Union's fee of $28 per item will be deducted from the Return Guard limit.</p>
+                <p>We pay overdrafts at our discretion, which means <strong>we do not guarantee</strong> that we will always authorize and pay any type of transaction. If we <strong>do not</strong> authorize and pay an overdraft, your transaction will be declined.</p>
+                <p>Call us at (478) 745 – 0494 or come by a branch today to learn more about these services.</p>
+
+                <p>Ready to sign up? To opt in or out of extended coverage with your debit card, please <a href="mailto:msoq@cgrcu.org" title="Contact a Member Service Rep">contact</a> a member service representative.</p>
+
+                <table class="overdraft-table" role="presentation">
+                    <thead>
+                        <td>Transactions Covered with Overdraft Privilege</td>
+                        <td>Standard Coverage (No action required)</td>
+                        <td>Extended Coverage (Your consent required)*</td>
+                        <td class="no-border">If you would like to select Extended Coverage for future transactions:</td>
+                    </thead>
+
+                    <tr>
+                        <td>Checks</td>
+                        <td>X</td>
+                        <td>X</td>
+                        <td class="no-border">Call us at: (478) 745 - 0494</td>
+                    </tr>
+
+                    <tr>
+                        <td>ACH – Automatic Debits</td>
+                        <td>X</td>
+                        <td>X</td>
+                        <td class="no-border"><a href="mailto:msoq@cgrcu.org" title="Contact a Member Service Rep">Contact</a> a member service representative.</td>
+                    </tr>
+
+                    <tr>
+                        <td>Recurring Debit Card Payments</td>
+                        <td>X</td>
+                        <td>X</td>
+                        <td class="no-border"><a href="https://www.cgrcu.org/pages/locations.php">ATM Locations</a></td>
+                    </tr>
+
+                    <tr>
+                        <td>Online Bill Pay Items</td>
+                        <td>X</td>
+                        <td>X</td>
+                        <td class="no-border"></td>
+                    </tr>
+
+                    <tr>
+                        <td>Online Banking Transfers</td>
+                        <td>X</td>
+                        <td>X</td>
+                        <td class="no-border"></td>
+                    </tr>
+
+                    <tr>
+                        <td>ATM Withdrawals</td>
+                        <td></td>
+                        <td>X*</td>
+                        <td class="no-border"></td>
+                    </tr>
+
+                    <tr>
+                        <td>Everyday Debit Card Transactions</td>
+                        <td></td>
+                        <td>X*</td>
+                        <td class="no-border"></td>
+                    </tr>
+
+                </table>
+
+                <div class="clearfix"></div>
+
+                <p>You can discontinue the Overdraft Protection at any time by contacting us at (478) 745 – 0494.</p>
+
+                <p><a href="https://www.cgrcu.org/pages/contact-us.php">Report Lost or Stolen Card</a></p>
+                <p>The consumer must repay any overdraft on his or her account within 30 days.&nbsp; If the account remains negative after 30 days, account may be closed at the Credit Union's discretion.
+                    CGR Credit Union will cover overdrafts on eligible accounts at their discretion up to $329.95; any amount over the limit will not be covered by the credit union.</p>
+            </div>
+            <!--end eight columns-->
+
+            <div class="three columns sidebar">
+                <?php include('../includes/accounts-sidebar.php'); ?>
+            </div>
+            <!--end four columns sidebar-->
+        </div>
+    </main>
 
 
 
 
-<?php include ('../includes/footer.php');?>
 
-<!--JAVASCRIPT INCLUDES-->
+
+    <?php include('../includes/footer.php'); ?>
+
+    <!--JAVASCRIPT INCLUDES-->
 
 
 </body>
