fa9c7e508f5b04a6c0a7354135093aa842b45e9d
Dillon Ellis <dellis@fcul.com>
2022-08-11 16:20:45 -0400

    original money market page

---
+++ pages/money-market.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<title>CGR Credit Union - Money Market</title>
+
+<!--META DATA-->
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<meta name="description" content="Like all credit unions, CGR Credit Union is a not-for-profit financial cooperative. When you join the credit union, you become a shareholder – or an owner – of a very unique financial institution. Earnings above the required reserves are returned to you, in the form of lower interest rates on loans and competitive dividends on savings.">
+<meta name="keywords" content=""/>
+
+<?php include ('../includes/head.php'); ?>
+
+</head>
+
+<body>
+<?php include_once("../includes/analyticstracking.php") ?>
+<?php include ('../includes/header.php');?>
+
+<main id="main" class="content page">
+
+<div class="container">
+
+<div class="nine columns">
+<h1 class="page-title">Money Market Accounts</h1>
+
+<p>With a low minimum balance, the Capital Share Account (Money Market account) gives you liquidity, instant access and a high return on your investment.  Dividends are paid monthly.</p>
+
+<div class="twelve columns">
+
+<div class="five columns">
+<ul>
+<li>Minimum deposit of $500</li>
+<li>Maximum of 3 withdrawals per month</li>
+<li>Excessive withdrawal fee of $5 per withdrawal if more than 3 per month</li>
+</ul>
+
+<br/>
+
+<p>Other features include:</p>
+
+<ul>
+<li>Access through <a href="https://www.cgrcu.org/pages/timely-teller.php">Timely Teller</a>, our 24 hour telephone account access</li>
+<li>E-Branch/Home Banking access</li>
+<li>Monthly statements</li>
+</ul>
+
+</div>
+
+<div class="six columns">
+<p align="center"><img src="../images/pages/money-market.jpg" alt="Money Market" title="Money Market"></p>
+</div>
+
+</div>
+
+<br>
+
+<p><a href="https://www.cgrcu.org/pages/share-rates.php">View Our Rates</a><br/>
+<a href="https://www.cgrcu.org/pages/contact-us.php">Contact Us</a></p>
+
+<p>Please <a href="https://www.cgrcu.org/pages/contact-us.php">contact us</a> for more information on payroll deduction.</p>
+
+<p><cite>* The Credit Union provides no legal advice to members, and provides the foregoing information from a reliable resource to give our members a basic understanding of these services. You should consult with your tax or legal advisor regarding any particulars and the current status of applicable federal and state laws. IRA's are insured separately by NCUA up to $250,000.</cite></p>
+
+ 
+</div><!--end eight columns-->
+
+<div class="three columns sidebar">
+<?php include ('../includes/accounts-sidebar.php'); ?>
+</div><!--end four columns sidebar-->
+	</div>
+</main>
+
+
+
+
+
+
+<?php include ('../includes/footer.php');?>
+
+<!--JAVASCRIPT INCLUDES-->
+
+
+</body>
+
+</html>
