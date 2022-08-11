1e9037027a78c8e1c0de04fea71cd2fcccd42adb
Dillon Ellis <dellis@fcul.com>
2022-08-11 16:12:15 -0400

    original club accounts page

---
+++ pages/club-accounts.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<title>CGR Credit Union - Club Accounts</title>
+
+<!--META DATA-->
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<meta name="description" content="Like all credit unions, CGR Credit Union is a not-for-profit financial cooperative. When you join the credit union, you become a shareholder – or an owner – of a very unique financial institution. Earnings above the required reserves are returned to you, in the form of lower interest rates on loans and competitive dividends on savings.">
+<meta name="keywords" content=""/>
+
+<?php include ('../includes/head.php'); ?>
+
+</head>
+
+<body>
+<?php include_once("../includes/analyticstracking.php") ?>
+<?php include ('../includes/header.php');?>
+
+<main id="main" class="content page">
+
+<div class="container">
+
+<div class="nine columns">
+<h1 class="page-title">Club Accounts</h1>
+
+<p>At CGR Credit Union, you can fund your club account through <a href="https://www.cgrcu.org/pages/payroll-deduction.php">payroll deduction</a>, automatic transfer, or cash deposits.  Our club accounts make saving easy.</p>  
+
+<div id="club-accounts-accordion" class="accordion">
+
+<h3 class="df-stitle fw600">Christmas Club</h3>
+
+<div><p>The holidays are an expensive time; start planning early with a Christmas Club account.  You can deposit any amount you want as often as you like.  Your balance will earn you dividends.  Save a little each month – we’ll send you a check in November, it’s just that easy!</p></div>
+
+<h3 class="df-stitle fw600">Vacation Club</h3>
+
+<div><p>Everybody needs a vacation now and then!  Save for your next getaway with a Vacation Club account.  You can deposit any amount you like as often as you want; your balance will earn you dividends.  Save a little each month – when you are ready to hit the road, the money is ready for you!</p></div>
+
+</div><!--end club-accounts-accordion-->
+
+<p class="df-stitle">Features included in the Club Account include:</p>
+
+<ul>
+<li>A competitive dividend <a href="https://www.cgrcu.org/pages/share-rates.php">rate</a></li>
+<li>No minimum balance required</li>
+<li>Access through <a href="https://www.cgrcu.org/pages/timely-teller.php">Timely Teller</a>, our 24 hour telephone account access</li>
+<li>E-Branch/Home Banking access</li>
+<li>Quarterly statement</li>
+<li><a href="https://www.cgrcu.org/pages/payroll-deduction.php">Payroll deduction</a> for convenient automatic deposits</li>
+</ul>
+
+<p>Please <a href="https://www.cgrcu.org/pages/contact-us.php">contact us</a> if you have any questions about Club Accounts.</p> 
+
+<p class="df-stitle fw600"><a href="https://cgrcu.org/calculators/savings/sc.html" target="_blank" style="color: white;text-decoration: underline;">Savings Calculator</a></p>
+
+<p><a href="https://www.cgrcu.org/pages/payroll-deduction.php">Payroll Deduction</a> is the convenient way to save for the future. You can set aside a certain amount from each paycheck to be automatically deposited into any of your accounts. Click here to access the <a href="https://www.cgrcu.org/pages/payroll-deduction.php">payroll deduction form</a>.</p>
+
+<p>Please <a href="https://www.cgrcu.org/pages/contact-us.php">contact us</a> for more information on <a href="https://www.cgrcu.org/pages/payroll-deduction.php">payroll deduction</a>.</p>
+
+<p>The National Credit Union Administration (NCUA) provides insurance coverage of up to $250,000 for your Credit Union account(s). To learn more about the type of accounts and the total amounts that the NCUA may insure visit this <a href="#" data-remodal-target="ncua1">link directly to NCUA</a>.</p>
+
+<p align="center"><img src="../images/pages/club-account.jpg" alt="Club Accounts" title="Club Accounts"></p>
+
+
+</div><!--end eight columns-->
+
+<div class="three columns sidebar">
+<?php include ('../includes/accounts-sidebar.php'); ?>
+</div><!--end four columns sidebar-->
+	</div>
+</main>
+
+
+
+
+
+
+<?php include ('../includes/footer.php');?>
+
+<!--JAVASCRIPT INCLUDES-->
+
+
+</body>
+
+</html>
