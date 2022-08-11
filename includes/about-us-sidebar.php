fba8b56b9d1c61f79747f3b0bb98641ae489974d
Dillon Ellis <dellis@fcul.com>
2022-08-11 16:28:55 -0400

    remove new bylaws from side bar nav

--- includes/about-us-sidebar.php
+++ includes/about-us-sidebar.php
@@ -1,11 +1,10 @@
 <aside class="default-sidebar about-us-sidebar">
-<ul>
-<li><a href="https://www.cgrcu.org/pages/locations.php">ATM Locations</a></li>
-<li><a href="https://www.cgrcu.org/pages/who-we-are.php">Who We Are</a></li>
-<li><a href="https://www.cgrcu.org/pages/who-can-join.php">Who Can Join</a></li>
-<li><a href="https://www.cgrcu.org/pages/contact-us.php">Contact Us</a></li>
-<li><a href="https://www.cgrcu.org/pages/usa-patriot.php">USA Patriot Act</a></li>
-<li><a href="https://www.cgrcu.org/pages/privacy-policy.php">Privacy Policy</a></li>
-<li><a href="https://www.cgrcu.org/pages/bylaws.php">New Bylaws</a></li>
-	</ul>
+    <ul>
+        <li><a href="https://www.cgrcu.org/pages/locations.php">ATM Locations</a></li>
+        <li><a href="https://www.cgrcu.org/pages/who-we-are.php">Who We Are</a></li>
+        <li><a href="https://www.cgrcu.org/pages/who-can-join.php">Who Can Join</a></li>
+        <li><a href="https://www.cgrcu.org/pages/contact-us.php">Contact Us</a></li>
+        <li><a href="https://www.cgrcu.org/pages/usa-patriot.php">USA Patriot Act</a></li>
+        <li><a href="https://www.cgrcu.org/pages/privacy-policy.php">Privacy Policy</a></li>
+    </ul>
 </aside>
