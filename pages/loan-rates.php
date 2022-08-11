b4df53c7ec38c2c7067f182565b500708e5fcd9c
Dillon Ellis <dellis@fcul.com>
2022-08-11 13:37:11 -0400

    original loan rates page

---
+++ pages/loan-rates.php
@@ -0,0 +1,397 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>CGR Credit Union - Loan Rates</title>
+
+    <!--META DATA-->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Like all credit unions, CGR Credit Union is a not-for-profit financial cooperative. When you join the credit union, you become a shareholder – or an owner – of a very unique financial institution. Earnings above the required reserves are returned to you, in the form of lower interest rates on loans and competitive dividends on savings.">
+    <meta name="keywords" content="" />
+
+    <?php include('../includes/head.php'); ?>
+
+</head>
+
+<body>
+    <?php include_once("../includes/analyticstracking.php") ?>
+    <?php include('../includes/header.php'); ?>
+
+    <main id="main" class="content page">
+
+        <div class="container">
+
+            <div class="nine columns">
+                <h1 class="page-title">Loan Rates</h1>
+
+                <p class="aligncenter df-mtitle fw700">Current Loan Rates as of July 2, 2022</p>
+
+                <table id="loan-rates-table" role="presentation">
+
+                    <tr class="head">
+                        <td>New and Used Automobiles</td>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <td class="subhead">Current Year (Less Than 15,000 miles)</td>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <td class="subhead">Term</td>
+                        <td class="subhead">Annual Percentage Rate (APR)</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 36 months</td>
+                        <td>3.30% to 15.05%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 48 months</td>
+                        <td>3.55% to 15.55%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 60 months</td>
+                        <td>3.55% to 16.05%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 66 months</td>
+                        <td>3.85% to 16.55%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 72 months</td>
+                        <td>4.04% to 12.05%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 84 months</td>
+                        <td>4.60% to 14.30%*</td>
+                    </tr>
+
+                    <tr class="head">
+                        <td>Used Current Year + 1-3 year old</td>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <td class="subhead">Term</td>
+                        <td class="subhead">Annual Percentage Rate (APR)</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 36 months</td>
+                        <td>3.85% to 15.90%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 48 months</td>
+                        <td>3.85% to 16.10%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 60 months</td>
+                        <td>3.85% to 16.40%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 66 months</td>
+                        <td>4.15% to 16.65%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 72 months</td>
+                        <td>4.45% to 13.90%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 84 months</td>
+                        <td>5.55% to 8.30%*</td>
+                    </tr>
+
+
+                    <tr class="head">
+                        <td>Used 4-5 Years Old</td>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <td class="subhead">Term</td>
+                        <td class="subhead">Annual Percentage Rate (APR)</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 36 months</td>
+                        <td>4.95% to 16.30%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 48 months</td>
+                        <td>5.25% to 16.55%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 60 months</td>
+                        <td>5.45% to 16.80%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 66 months</td>
+                        <td>5.60% to 14.30%*</td>
+                    </tr>
+
+                    <tr class="head">
+                        <td>Used 6-8 Years Old</td>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <td class="subhead">Term</td>
+                        <td class="subhead">Annual Percentage Rate (APR)</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 36 months</td>
+                        <td>6.30% to 17.10%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 48 months</td>
+                        <td>6.50% to 17.30%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 60 months</td>
+                        <td>6.80% to 17.55%*</td>
+                    </tr>
+
+                    <tr class="head">
+                        <td>Other Collateral Loans</td>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <td>New</td>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <td class="subhead">Term</td>
+                        <td class="subhead">Annual Percentage Rate (APR)</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 36 months</td>
+                        <td>6.15% to 15.90%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 48 months</td>
+                        <td>6.15% to 16.90%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 60 months</td>
+                        <td>6.15% to 17.90%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 66 months</td>
+                        <td>7.00% to 14.15%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 72 months</td>
+                        <td>7.40% to 10.85%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 84 months</td>
+                        <td>8.00% to 11.95%*</td>
+                    </tr>
+
+                    <tr class="head">
+                        <td>Used</td>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <td class="subhead">Term</td>
+                        <td class="subhead">Annual Percentage Rate (APR)</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 36 months</td>
+                        <td>7.25% to 17.25%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 48 months</td>
+                        <td>7.50% to 17.75%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 60 months</td>
+                        <td>7.75% to 18.00%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 66 months</td>
+                        <td>8.00% to 15.00%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 72 months</td>
+                        <td>8.45% to 13.60%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 84 months</td>
+                        <td>9.95% to 15.00%*</td>
+                    </tr>
+
+                    <tr class="head">
+                        <td>Personal Loans</td>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <td class="subhead">Term</td>
+                        <td class="subhead">Annual Percentage Rate (APR)</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 36 months</td>
+                        <td>9.00% to 18.00%*</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 48 months</td>
+                        <td>10.00% to 17.50%*</td>
+                    </tr>
+
+                    <tr class="head">
+                        <td>Share Secured Loans</td>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <td class="subhead">Term</td>
+                        <td class="subhead">Annual Percentage Rate (APR)</td>
+                    </tr>
+
+                    <tr>
+                        <td>up to 60 months</td>
+                        <td>Dividend Rate +3.00%</td>
+                    </tr>
+
+                    <tr class="head">
+                        <td>First Mortgage Loans</td>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <td class="subhead">Term</td>
+                        <td class="subhead">Annual Percentage Rate (APR)</td>
+                    </tr>
+
+                    <tr>
+                        <td>Up to 10 Years</td>
+                        <td>Secondary Market Pricing</td>
+                    </tr>
+
+                    <tr>
+                        <td>Up to 15 Years</td>
+                        <td>Secondary Market Pricing</td>
+                    </tr>
+
+                    <tr>
+                        <td>Up to 20 Years</td>
+                        <td>Secondary Market Pricing</td>
+                    </tr>
+
+                    <tr>
+                        <td>Up to 30 Years</td>
+                        <td>Secondary Market Pricing</td>
+                    </tr>
+
+                    <tr class="head">
+                        <td>Fixed Home Equity Loans</td>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <td class="subhead">Loan to Value (LTV)</td>
+                        <td class="subhead">Annual Percentage Rate (APR)</td>
+                    </tr>
+
+                    <tr>
+                        <td>90% or Less of Appraised Value</td>
+                        <td>As low as 3.50%***</td>
+                    </tr>
+
+                    <tr>
+                        <td>More than 90% of Appraised Value</td>
+                        <td>As low as 4.50%***</td>
+                    </tr>
+
+                    <tr class="head">
+                        <td>Home Equity Lines of Credit</td>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <td class="subhead">Loan to Value (LTV)</td>
+                        <td class="subhead">Annual Percentage Rate (APR)</td>
+                    </tr>
+
+                    <tr>
+                        <td>80% or Less of Appraised Value</td>
+                        <td>As low as 4.75%**</td>
+                    </tr>
+
+                    <tr>
+                        <td>More than 80% of Appraised Value</td>
+                        <td>As low as 4.75%**</td>
+                    </tr>
+
+                </table>
+
+                <p class="fw700 aligncenter">Floor Rate of 4.00%****</p>
+
+                <div class="clearfix m30-t"></div>
+
+                <p><a href="https://www.cgrcu.org/calculators/APR/aprc.htm" target="_blank">Click here for the APR calculator</a>.</p>
+
+                <p>* All loans subject to credit approval. Some restrictions may apply. Your APR may vary depending on your creditworthiness. Please ask a loan officer for details. Any increase in the APR will result in more payments of the same amount. Rates and terms subject to change without notice.</p>
+
+                <p>**Maximum amount financed is 95% of appraised value. All loans subject to credit approval. Some restrictions may apply. Your APR may vary depending on your creditworthiness. Please ask a loan officer for details. Rates and terms subject to change without notice.</p>
+
+                <p>*** As low as rate is for 60 month term. Creditworthiness and length of time financed (term) determines APR. Terms up to 180 months are available.</p>
+
+                <p>****Prime Rate is a variable rate; as it changes, the APR on your account will also change.</p>
+                </p>
+
+            </div>
+            <!--end eight columns-->
+
+            <div class="three columns sidebar">
+                <?php include('../includes/loans-sidebar.php'); ?>
+            </div>
+            <!--end four columns sidebar-->
+        </div>
+    </main>
+
+    <?php include('../includes/footer.php'); ?>
+
+    <!--JAVASCRIPT INCLUDES-->
+
+
+</body>
+
+</html>
