8190c51a9bd54b354dc62574c397d171b23f0d21
Dillon Ellis <dellis@fcul.com>
2022-08-12 09:05:50 -0400

    rate update

--- promo/finance-auto-loan.php
+++ promo/finance-auto-loan.php
@@ -26,7 +26,7 @@
 
                 <h1 class="page-title">Finance your Auto Loan through CGR and get $75 Gas Cash</h1>
 
-                <p>In the market for a new or used car or truck? Finance it with us and we’ll fill up your gas tank. Our rates start as low as 2.40% APR*. Fit your payment into your budget with payment options from weekly, bi-weekly, semi-monthly, or monthly payments. We make it easy to finance your new car.</p>
+                <p>In the market for a new or used car or truck? Finance it with us and we’ll fill up your gas tank. Our rates start as low as 3.30% APR*. Fit your payment into your budget with payment options from weekly, bi-weekly, semi-monthly, or monthly payments. We make it easy to finance your new car.</p>
 
                 <a class="bold-btn" href="https://www.secure-cgrcu.org/Centryx/servlet/com.sos.webteller.onlineforms.AcctPassApp?SOSSessionID=!_v2ewd6i84EA4DZbtJw6nRyVMx6CspLeHanaZZ/JKWEYjGfDodwshLmd2YPI2UhzR1OfriTXu1v4yhm5fsl/jdgoz0zpf5JteXg2bKHRb+9UDWEgVSn30pjCe2v_!&Type=!_v2eHw56oYY+GPl/dtu9xvuftJ4kNWV2CufaNa2PxfDT2gO0sOu/UEQ2a6GA0C9Fmp4oCd02nLy0e2v_!" target="_blank">Apply Now</a>
 
@@ -34,7 +34,7 @@
                     <br>
                     *Annual Percentage Rate
                     <br>
-                    **A limited time offer and does not apply to existing CGRCU loans. Applicant must qualify under CGR Credit Union’s normal underwriting guidelines. Gas Cash available to members financing over $20,000 on a new or used vehicle. Lowest rate available is 2.40% APR and may vary depending on loan type, term, and applicant credit history. Length of pre-approval may vary depending on applicant credit history. Program rates, terms, and conditions are subject to change without notice.
+                    **A limited time offer and does not apply to existing CGRCU loans. Applicant must qualify under CGR Credit Union’s normal underwriting guidelines. Gas Cash available to members financing over $20,000 on a new or used vehicle. Lowest rate available is 3.30% APR and may vary depending on loan type, term, and applicant credit history. Length of pre-approval may vary depending on applicant credit history. Program rates, terms, and conditions are subject to change without notice.
                 </p>
 
 
