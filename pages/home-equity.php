e940b29e81e8216d79e08099619782c0c94630b9
Dillon Ellis <dellis@fcul.com>
2022-09-01 10:22:38 -0400

    original home equity page

---
+++ pages/home-equity.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<title>CGR Credit Union - Home Equity</title>
+
+<!--META DATA-->
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<meta name="description" content="Like all credit unions, CGR Credit Union is a not-for-profit financial cooperative. When you join the credit union, you become a shareholder – or an owner – of a very unique financial institution. Earnings above the required reserves are returned to you, in the form of lower interest rates on loans and competitive dividends on savings.">
+<meta name="keywords" content=""/>
+
+<?php include ('../includes/head.php'); ?>
+
+</head>
+
+<body>
+<?php include_once("../includes/analyticstracking.php") ?>
+<?php include ('../includes/header.php');?>
+
+<main id="main" class="content page">
+
+<div class="container">
+
+<div class="nine columns">
+ 	
+ 
+<h1 class="page-title">Home Equity Loans</h1>
+
+<div class="twelve columns">
+
+<div class="seven columns">
+<p>Turn your home ownership into a more rewarding experience with a Home Equity Line of Credit from CGR Credit Union.  If you have built solid equity in your home, a home equity loan is one of the smartest ways to borrow money.  Capitalize on the value you’ve built up in your property with cash for home improvements, educational expenses, debt consolidation, medical expenses and more.  </p>
+
+<p>A Home Equity Line of Credit from CGR Credit Union carries a competitive interest rate and the personalized service you’re accustomed to receiving from your credit union.</p>
+
+<p>Take advantage of our <a href="../promo/home-equity.php">HELOC promotion</a> going on NOW with a 2 year Introductory APR rate of 2.9%*.</p>
+
+<p><a id="loanappLink" href ="#" onclick="postLink('https://www.secure-cgrcu.org/Centryx/servlet/com.sos.webteller.WebTellForms?Page=loanapp'); return false;" target="_blank">Apply Now</a> or call <a href="tel:4787450494">(478) 745–0494</a></p>
+</div>
+
+<div class="four columns">
+<p align="center"><img src="../images/pages/heloc.jpg" alt="HELCO" title="HELOC"></p>
+</div>
+
+</div>
+
+<p><a href="https://www.cgrcu.org/pages/loan-rates.php">Loan Rates</a></p>
+<p><a href="https://cgrcu.org/calculators/refinance/rfc.html" target="_blank">Refinance Calculator</a></p>
+	
+	<p style="font-size: 12px;
+    font-style: italic;
+    line-height: normal;">Disclaimer:<br>
+*Annual Percentage Rate<br><br>
+
+**A limited time offer. Rates do not apply to existing CGRCU home equity loans. Must be primary residence of applicant. The promotional rate of 2.9% APR for 24 months begins from the date of closing with an initial loan advance of $10,000. The 2.9% Introductory APR is fixed during the Introductory Period for all credit advances taken during such period. Minimum loan amount of $15,000; maximum loan amount of $100,000. CGR Credit Union will pay closing costs on new Home Equity Line of Credit applications. HELOCs have a 5-year draw period, and a 15-year repayment period. Credit and collateral are subject to approval. Credit application required.<br><br>
+
+After the Introductory Period ends, the APR is variable and is based upon an index and a margin. The variable APR will apply to the outstanding balance as of the end of the Introductory Period. Maximum APR is 18.0%; minimum APR is 4.0%. Margin, rate and payment amount may vary based on property value, loan amount, creditworthiness and other factors. Property insurance is required; flood insurance may be required depending on the location of the collateral.
+</p>
+
+
+</div><!--end eight columns-->
+
+<div class="three columns sidebar">
+<?php include ('../includes/loans-sidebar.php'); ?>
+</div><!--end four columns sidebar-->
+	</div>
+</main>
+
+
+
+
+
+
+<?php include ('../includes/footer.php');?>
+
+<!--JAVASCRIPT INCLUDES-->
+
+
+</body>
+
+</html>
